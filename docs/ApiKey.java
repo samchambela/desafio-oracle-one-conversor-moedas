44e7f92e83caa21121e050bff1b234853c208fc5
samchambela <chambela.sam@gmail.com>
2026-01-15 14:39:09 -0300

    Ajuste nos próximos passos: arquivo "anotações.md"

---
+++ docs/ApiKey.java
@@ -0,0 +1,12 @@
+package api;
+
+/**
+ * Arquivo de EXEMPLO.
+ * Copie este arquivo e renomeie para ApiKeys.java dentro de src/api
+ * NÃO versione o ApiKeys.java
+ */
+
+public class ApiKey {
+    public static final String EXCHANGE_RATE =
+            "<SUA_CHAVE_API>";
+}
